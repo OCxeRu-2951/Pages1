950d9fb2b0b0135766136a2d32a71f5acd486484
NaCl <k2000.xxx0@gmail.com>
2024-10-23 12:02:36 +0900

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme) {
+        $('body').toggleClass('light-theme', savedTheme === 'light');
+    }
+
+    // サイドバートグルボタン
     $('#toggleButton').click(function() {
         $('#sidebar').toggleClass('active');
     });
 
+    // テーマ切替ボタン
     $('#themeToggle').click(function() {
         $('body').toggleClass('light-theme');
+        
+        if ($('body').hasClass('light-theme')) {
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.setItem('theme', 'dark');
+        }
     });
 });
